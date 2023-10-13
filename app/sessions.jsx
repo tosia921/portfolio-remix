ac6673c9f1bd82b2c8863d89e0d3f4d60f90e195
Tomasz Posiadala <tomasz@fostr.io>
2023-10-13 23:46:46 +0100

    test

--- app/sessions.jsx
+++ app/sessions.jsx
@@ -1,16 +1,11 @@
-import { createCookie, createFileSessionStorage } from '@remix-run/node' // or cloudflare/deno
+import { createCookieSessionStorage } from '@remix-run/node'
 
-// In this example the Cookie is created separately.
-const sessionCookie = createCookie('__session', {
-	secrets: ['r3m1xr0ck5'],
-	sameSite: true,
-})
-
-const { getSession, commitSession, destroySession } = createFileSessionStorage({
-	// The root directory where you want to store the files.
-	// Make sure it's writable!
-	dir: '/app/sessions',
-	cookie: sessionCookie,
+const { getSession, commitSession, destroySession } = createCookieSessionStorage({
+	cookie: {
+		name: '__session',
+		sameSite: 'lax',
+		secrets: ['s3cr3ts'],
+	},
 })
 
 export { getSession, commitSession, destroySession }
