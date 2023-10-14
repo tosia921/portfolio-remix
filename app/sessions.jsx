fa3e48d56516665bf237a14594bb6437c9086653
Tomasz Posiadala <tomasz@fostr.io>
2023-10-14 14:40:15 +0100

    upstash sessions

--- app/sessions.jsx
+++ app/sessions.jsx
@@ -1,16 +1,37 @@
-import { createCookie, createKvSessionStorage } from '@vercel/remix' // or cloudflare/deno
+// Development setup
 
-// In this example the Cookie is created separately.
+// import { createCookieSessionStorage } from '@vercel/remix'
+
+// const { getSession, commitSession, destroySession } = createCookieSessionStorage({
+// 	cookie: {
+// 		name: '__session',
+// 		sameSite: 'lax',
+// 		secrets: ['s3cr3ts'],
+// 	},
+// })
+
+// export { getSession, commitSession, destroySession }
+
+// sessions/upstash.server.ts
+
+// sessions.server.ts
+
+import { createUpstashSessionStorage } from '~/sessions/upstash.server'
+import { createCookie } from '@vercel/remix'
+
+// This will set the length of the session.
+// For the example we use a very short duration to easily demonstrate its functionally.
+const EXPIRATION_DURATION_IN_SECONDS = 44200 // half a day
+
+const expires = new Date()
+expires.setSeconds(expires.getSeconds() + EXPIRATION_DURATION_IN_SECONDS)
+
 const sessionCookie = createCookie('__session', {
-	secrets: ['r3m1xr0ck5'],
+	secrets: ['r3m1xr0ck1'],
 	sameSite: true,
+	expires,
 })
 
-const { getSession, commitSession, destroySession } = createKvSessionStorage({
-	// The root directory where you want to store the files.
-	// Make sure it's writable!
-	dir: '/app/sessions',
-	cookie: sessionCookie,
-})
+const { getSession, commitSession, destroySession } = createUpstashSessionStorage({ cookie: sessionCookie })
 
 export { getSession, commitSession, destroySession }
