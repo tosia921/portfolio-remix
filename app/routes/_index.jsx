d0a2e9fcd22d8b967b2e0364095a5c23d132148d
Tomasz Posiadala <tomasz@fostr.io>
2023-10-21 16:04:54 +0100

    ss

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
 		<div className="flex min-h-screen flex-col items-center justify-center">
 			<div className="rotate-[-3deg]">
 				<h1 className="font-heading text-5xl font-black leading-[1]">
-					welcome!!! i'm <span className="text-primary">Tomasz</span>
+					welcome!!!!! i'm <span className="text-primary">Tomasz</span>
 				</h1>
 				<p className="font-heading text-7xl font-black leading-[0.9]">and I make</p>
 				<p className="font-heading text-7xl font-black leading-[0.4]">websites</p>
